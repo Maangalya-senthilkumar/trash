8e2bd8658db678bcdc4152e63d0dec12696eb68c
Maangalya-senthilkumar <134730211+Maangalya-senthilkumar@users.noreply.github.com>
2024-12-16 23:33:22 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,10 @@
 // JavaScript for Trash Tracker
 
-// Handle Trash Prediction
-document.getElementById("trashForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const trashImage = document.getElementById("trashImage").files[0];
+function openGradio() {
+    // Replace this with your Gradio app URL
+    window.location.href = "https://67f8b2dcae8b1b4634.gradio.live";
+}
 
-    if (!trashImage) {
-        alert("Please upload an image!");
-        return;
-    }
-
-    // Redirect to Gradio URL
-    window.open("https://67f8b2dcae8b1b4634.gradio.live");
     
     // Simulate earning credits
     showPopup();
